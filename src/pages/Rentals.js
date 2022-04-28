7c2af7fcd81f50b8258d3676b612f091f125458b
Brian Chege Gichuhi <brianchegegichuhi@gmail.com>
2022-04-28 05:20:45 +0300

    Rentals page topbanner

--- src/pages/Rentals.js
+++ src/pages/Rentals.js
@@ -1,18 +1,24 @@
 import React from "react";
 import "./Rentals.css";
 import { Link, useLocation } from "react-router-dom";
+import logo from '../images/airbnbRed.png';
+import { ConnectButton } from "web3uikit";
 
 const Rentals = () => {
   const { state: searchFilters } = useLocation();
   return (
     <>
-     <Link to="/"> Home </Link>
-     {
-        searchFilters && 
+      <div className="topBanner">
         <div>
-          {searchFilters.destination}
+          <Link to='/'>
+            <img className="logo" src={logo} alt="logo" />
+          </Link>
         </div>
-     }
+        <div className="searchReminder"></div>
+        <div className="lrContainers">
+          <ConnectButton />
+        </div>
+      </div>
     </>
   );
 };
