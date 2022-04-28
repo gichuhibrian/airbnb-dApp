8fc7d19887f9da714012109e71f2f0526883647f
Brian Chege Gichuhi <brianchegegichuhi@gmail.com>
2022-04-28 04:26:39 +0300

    styling the homepage and adding the wallet connect button

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,29 @@
 import React from "react";
 import "./Home.css";
-import { Link } from "react-router-dom";
+import bgImage from "../images/frontpagebg.png";
+import logo from '../images/airbnb.png';
+import { ConnectButton } from "web3uikit";
 
 const Home = () => {
 
   return (
     <>
-      <Link to="/rentals"> Rentals </Link>
+      <div className="container" style={{backgroundImage: `url(${bgImage})`}}>
+        <div className="containerGradinet"></div>
+      </div>
+      <div className="topBanner">
+        <div>
+          <img className="logo" src={logo} alt="logo" />
+        </div>
+        <div className="tabs">
+          <div className="selected">Places To Stay</div>
+          <div>Experiences</div>
+          <div>Online Experiences</div>
+        </div>
+        <div className="lrContainers">
+          <ConnectButton />
+        </div>
+      </div>
     </>
   );
 };
