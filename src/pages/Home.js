70dd57c74116b8d49a9864881af8b3763250b406
Brian Chege Gichuhi <brianchegegichuhi@gmail.com>
2022-04-28 04:45:39 +0300

    Added searchbar in homepage

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,7 +2,7 @@ import React from "react";
 import "./Home.css";
 import bgImage from "../images/frontpagebg.png";
 import logo from '../images/airbnb.png';
-import { ConnectButton } from "web3uikit";
+import { ConnectButton, Select, DatePicker, Input } from "web3uikit";
 
 const Home = () => {
 
@@ -22,6 +22,64 @@ const Home = () => {
         </div>
         <div className="lrContainers">
           <ConnectButton />
+        </div>
+      </div>
+      <div className="tabContent">
+        <div className="searchFields">
+          <div className="inputs">
+            Location
+            <Select 
+              defaultOptionIndex={0}
+              onChange={(data) => console.log(data.label)}
+              options={[
+                {
+                  id: "ny",
+                  label: "New York",
+                },
+                {
+                  id: "la",
+                  label: "Los Angeles",
+                },
+                {
+                  id: "ch",
+                  label: "Chicago",
+                },
+                {
+                  id: "sf",
+                  label: "San Francisco",
+                }
+              ]}
+            />
+          </div>
+          <div className="vl" />
+
+          <div className="inputs">
+            Check In
+            <DatePicker 
+              id="checkIn"
+              onChange={(event) => console.log(event.date)}
+            />
+          </div>
+          <div className="vl" />
+
+          <div className="inputs">
+            Check Out
+            <DatePicker 
+              id="checkOut"
+              onChange={(event) => console.log(event.date)}
+            />
+          </div>
+          <div className="vl" />
+
+          <div className="inputs">
+            Guests
+            <Input 
+              value={2}
+              name="AddGuests"
+              type="number"
+              onChange={(event) => console.log(Number(event.target.value))}
+            />
+          </div>
         </div>
       </div>
     </>
