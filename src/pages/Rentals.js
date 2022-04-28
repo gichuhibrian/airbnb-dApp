49509a9b679854fd1fff9e2ff3aff884c3a3dc00
Brian Chege Gichuhi <brianchegegichuhi@gmail.com>
2022-04-28 05:31:34 +0300

    Adding search reminder from homepage

--- src/pages/Rentals.js
+++ src/pages/Rentals.js
@@ -2,7 +2,7 @@ import React from "react";
 import "./Rentals.css";
 import { Link, useLocation } from "react-router-dom";
 import logo from '../images/airbnbRed.png';
-import { ConnectButton } from "web3uikit";
+import { ConnectButton, Icon } from "web3uikit";
 
 const Rentals = () => {
   const { state: searchFilters } = useLocation();
@@ -14,7 +14,30 @@ const Rentals = () => {
             <img className="logo" src={logo} alt="logo" />
           </Link>
         </div>
-        <div className="searchReminder"></div>
+        <div className="searchReminder">
+          <div className="filter">
+            {searchFilters.destination}
+          </div>
+          <div className="vl" />
+
+          <div className="filter">
+            {`
+              ${searchFilters.checkIn.toLocaleString("default", {month: "short",})}
+              ${searchFilters.checkIn.toLocaleString("default", {day: "2-digit",})}
+              -
+              ${searchFilters.checkOut.toLocaleString("default", {month: "short",})}
+              ${searchFilters.checkOut.toLocaleString("default", {day: "2-digit",})}
+            `}
+          </div>
+          <div className="vl" />
+
+          <div className="filter">
+            {searchFilters.guests} {`${searchFilters.guests === 1 ? "Guest" : "Guests"}`}
+          </div>
+          <div className="searchFiltersIcon">
+            <Icon fill="#fff" name="filter" size={24} svg="search"/>
+          </div>
+        </div>
         <div className="lrContainers">
           <ConnectButton />
         </div>
